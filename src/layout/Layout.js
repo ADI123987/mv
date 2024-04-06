cbf0f12fe126c70391d738b6bd7fd1912e9f29a8
Aditya <adityakumarsingh123987@gmail.com>
2024-04-06 23:21:28 +0530

    metadata

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,6 +6,8 @@ import ScrollTop from "./ScrollTop";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import SideBar from "./header/SideBar";
+import Head from 'next/head';
+
 const Layout = ({ children, header, footer, singleMenu, dark }) => {
   useEffect(() => {
     animation();
@@ -14,6 +16,12 @@ const Layout = ({ children, header, footer, singleMenu, dark }) => {
 
   return (
     <Fragment>
+      <Head>
+        <title>MANEUVER VENTURES</title>
+        <meta name="description" content="Maneuver Ventures Is An Investment Banking Firm That Provides Advisory Services Such As Business Restructuring, Portfolio Management, And Mergers And Acquisitions. The Company Works With High-Performing Entrepreneurs And Potential Wealth Creators To Help Them Outperform The Current Market. With Our Deep Understanding Of The Sectors We Cover, We Are Uniquely Positioned To Provide Tailored And Responsible Solutions For Our Clients." />
+        <link rel="icon" href="public/assets/images/favicon.webp" />
+        {/* Add other metadata as needed */}
+      </Head>
       <VideoPopup />
       <ImageView />
       <div className="page-wrapper">
@@ -26,4 +34,5 @@ const Layout = ({ children, header, footer, singleMenu, dark }) => {
     </Fragment>
   );
 };
+
 export default Layout;
